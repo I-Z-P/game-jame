544040a770161f188159eddb36a031c482e10c20
jeli-t <jelitotomek@gmail.com>
2022-11-13 18:33:32 +0100

    add keyboard_assignments()

--- core/events.py
+++ core/events.py
@@ -2,11 +2,19 @@
 
 
 import pygame
+from config import DEV
+
+
+def keyboard_assignments(event):
+    if DEV: print('Pressed key in ascii:', event.key) 
+    pressed_keys = pygame.key.get_pressed()
+    if pressed_keys[pygame.K_ESCAPE]:
+        pygame.quit()
 
 
 def handle_events():
     for event in pygame.event.get():
-        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-            run = False
+        if event.type == pygame.QUIT:
             pygame.quit()
-        # keys = pygame.key.get_pressed()
+        if event.type == pygame.KEYDOWN:
+            keyboard_assignments(event)
