c3ac3907041a05eae805baed89712d026106f322
jeli-t <jelitotomek@gmail.com>
2022-11-14 21:14:29 +0100

    add delta_time

--- core/main.py
+++ core/main.py
@@ -4,17 +4,27 @@
 from config import *
 from events import *
 from render import *
+import time
 import pygame
 pygame.init()
 
 
+# pin update functions to run in each iteration here
+def update(delta_time):
+    # debug_msg(delta_time)
+    pass
+
+
 def game_loop(screen):
-    clock = pygame.time.Clock()
+    previous_time = time.time()
     while True:
-        clock.tick(TICKRATE)
         render(screen)
+        delta_time = time.time() - previous_time
+        previous_time = time.time()
+        update(delta_time)
         handle_events()
         pygame.display.update()
+
 
 def launch_the_game():
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
