5c8749a1b3e6e6d50c9120b4bc77a2039f0b3aa9
Naris404 <piotr.jakubczyk04@gmail.com>
2022-11-25 16:36:47 +0100

    Fix player movement

--- core/events.py
+++ core/events.py
@@ -17,10 +17,17 @@ def keyboard_assignments(event, player):
     if pressed_keys[pygame.K_UP]:
         player.move_up()
 
-
 def handle_events(player):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
         if event.type == pygame.KEYDOWN:
             keyboard_assignments(event, player)
+        # temporarly added in order to fix player movement
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                player.jump = False
+            elif event.key == pygame.K_RIGHT:
+                player.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                player.moving_left = False
--- player/Player.py
+++ player/Player.py
@@ -6,7 +6,7 @@ from pygame.locals import *
 from numpy import sign
 
 class Player():
-    def __init__(self, gravity=10, max_jumps=1, starting_position=[0,0], acceleration=[0.2,0], max_velocity=10, player_images=None):
+    def __init__(self, gravity=4, max_jumps=1, starting_position=[0,0], acceleration=[0.2,0], max_velocity=5, player_images=None):
         self.base_acceleration = acceleration
         self.max_velocity = abs(max_velocity)
         self.gravity = gravity
@@ -34,7 +34,7 @@ class Player():
             rect = pygame.Rect(0,0,10,10)
             return surfaces, rect
 
-    def collide(self, tiles): # return every tile that player collide with
+    def collide(self, tiles):
         collisions = []
         for tile in tiles:
             if self.rect.colliderect(tile):
@@ -56,35 +56,37 @@ class Player():
         dt *= 100 # normalize
         if abs(self.velocity.x) <= self.max_velocity: # if boundry of max velocity is not crossed
             if self.acceleration.x == 0: self.acceleration.x = self.base_acceleration[0] # if acceleration is turned off then turn it on
-            if self.moving_right: # if true move right
-                self.velocity += self.acceleration * dt # apply acceleretion
-            elif self.moving_left: # if true move left
+            if self.moving_right:
+                self.velocity += self.acceleration * dt
+            elif self.moving_left:
                 self.velocity -= self.acceleration * dt
             else:
-                self.acceleration.x = 0 # if player is not moving turn off acceleration
-        else: # if velocity has crossed boundry
+                if abs(self.velocity.x) < abs(self.acceleration.x): self.velocity.x = 0
+                else:
+                    self.velocity.x += -sign(self.velocity.x) * self.acceleration.x/self.gravity
+        else:
             self.acceleration.x = 0
-            self.velocity.x = sign(self.velocity.x) * self.max_velocity # set velocity to max speed
-        self.position.x += self.velocity.x * dt # update position
+            self.velocity.x = sign(self.velocity.x) * self.max_velocity
+        self.position.x += self.velocity.x * dt
         if self.jump:
-            if self.jumps < self.max_jumps and not self.pressing: # if number of jumps counter is not over maximal number of jumps and if user not pressing space
-                self.vertical_momentum = -8 # static jump for now
-                self.jumps += 1 # add to one jump counter
+            if self.jumps < self.max_jumps and not self.pressing:
+                self.vertical_momentum = -4 # static jump for now
+                self.jumps += 1
                 self.pressing = True # initialize space pressing
         else:
             self.pressing = False # user stopped pressing space
-        self.vertical_momentum += dt/10 # increase vertical momentum by tiny step
+        self.vertical_momentum += dt/self.gravity
         if self.vertical_momentum > self.gravity:
             self.vertical_momentum = self.gravity
-        self.position.y += self.vertical_momentum * dt # apply one step of gravity
-        self.rect.x = self.position.x # update position on a player
+        self.position.y += self.vertical_momentum * dt
+        self.rect.x = self.position.x
 
     def check_collisions(self, dt, tiles):
         collisions = self.collide(tiles)
         for tile in collisions:
             if self.moving_right:
-                self.position.x -= self.velocity.x * dt # back track to previous position
-                self.rect.x = self.position.x # on a player also
+                self.position.x -= self.velocity.x * dt
+                self.rect.x = self.position.x
             elif self.moving_left:
                 self.position.x += self.velocity.x * dt
                 self.rect.x = self.position.x
@@ -94,11 +96,11 @@ class Player():
             if self.vertical_momentum < 0:
                 self.position.y = tile.rect.bottom
                 self.rect.y = self.position.y
-                self.vertical_momentum = 0.5
+                self.vertical_momentum = self.gravity/10
             else:
                 self.position.y = (tile.rect.top - self.rect.h)
                 self.rect.y = self.position.y
-                self.jumps = 0 # if on ground clear jumps counting
+                self.jumps = 0
                 self.vertical_momentum = 0
 
     def render(self, screen):
